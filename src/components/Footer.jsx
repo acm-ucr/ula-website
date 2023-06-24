4554fe26e1a9967396de8a7e8ba75aa4d9248aed
menthy-wu <wumengxuan0728@gmail.com>
2023-06-24 01:52:28 -0700

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full bg-ula-lightGray flex items-center justify-between">
+      <div className="flex items-center">
+        <FaEnvelope className="text-ula-blue text-4xl m-3" />
+        <div className="flex flex-col m-2">
+          <p className="p-0 m-0 text-sm text-ula-darkGray">
+            Questions? Contact
+          </p>
+          <p className="p-0 m-0 text-sm font-bold text-ula-darkGray">
+            Neftali Watinkinson
+          </p>
+          <a
+            className="p-0 m-0 text-sm no-underline text-ula-blue"
+            href="mailto:neftaliw@ucr.edu"
+          >
+            neftaliw@ucr.edu
+          </a>
+        </div>
+      </div>
+      <img src="/bcoeLogo.png" className="w-1/5 mr-5" />
+    </div>
+  );
 };
 
 export default Footer;
