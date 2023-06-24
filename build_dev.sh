53abf3ccdf805a02fe0c15c95173a2889802416d
Divyank Shah <divyank.shah.2016@gmail.com>
2023-06-24 06:59:04 -0400

    add build scripts

---
+++ build_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SPECIFY PATH OF FOLDER HERE
+#cd /home/projects/..........
+
+# Builds the `dev` branch 
+res=$(git pull origin dev | grep Already)
+
+if [[ ${res} =~ 'Already' ]]
+then
+  echo 'Site already up to date'
+else
+  npm ci
+  npm run build
+fi
