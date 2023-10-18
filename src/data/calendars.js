bf26e5b3e17b46c9ac954a746a45d14b4336a077
Divyank Shah <divyank.shah.2016@gmail.com>
2023-10-18 15:03:09 -0700

    add python

--- src/data/calendars.js
+++ src/data/calendars.js
@@ -1,5 +1,12 @@
 export const calendars = [
   {
+    name: "Python (CS9A/B)",
+    color: "!bg-ula-yellow",
+    text: "text-ula-yellow",
+    border: "border-ula-yellow",
+    calendar: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_PYTHON,
+  },
+  {
     name: "Software (CS10A/B/C)",
     color: "!bg-ula-yellow",
     text: "text-ula-yellow",
