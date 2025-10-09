9d4637706012c6559d6dacc67ed54c19ed9e531f
Kevin Loritsch <kloritsch58@gmail.com>
2025-10-08 20:59:06 -0700

    add second half

--- src/data/CurrentULAsData.ts
+++ src/data/CurrentULAsData.ts
@@ -16,10 +16,17 @@ import KevinH from "@/public/ulas/kevinH.webp";
 import KevinL from "@/public/ulas/kevinL.webp";
 import Kyan from "@/public/ulas/kyan.webp";
 import Livayaa from "@/public/ulas/livayaa.jpeg";
+import Luis from "@/public/ulas/luis.jpg";
 import Mailyn from "@/public/ulas/mailyn.webp";
+import Marilyn from "@/public/ulas/marilyn.jpeg";
 import Natalie from "@/public/ulas/natalie.webp";
+import Nikhil from "@/public/ulas/nikhil.png";
+import Pratheek from "@/public/ulas/pratheek.jpg";
 import Ramya from "@/public/ulas/ramya.webp";
+import Riley from "@/public/ulas/riley.jpeg";
 import Sahana from "@/public/ulas/sahana.webp";
+import Sarbesh from "@/public/ulas/sarbesh.png";
+import Simon from "@/public/ulas/simon.webp";
 import Stanley from "@/public/ulas/stanley.webp";
 import { StaticImageData } from "next/image";
 
@@ -30,15 +37,6 @@ export interface ULA {
   desc: string;
 }
 
-/*
-{
-    name: "",
-    classes: "CS",
-    image: ,
-    desc: "",
-  },
-*/
-
 const ULAs: ULA[] = [
   {
     name: "Aaryan",
@@ -48,7 +46,7 @@ const ULAs: ULA[] = [
   },
   {
     name: "Adi",
-    classes: "CS061",
+    classes: "CS61",
     image: Adithya,
     desc: "Hi hi ! I'm Adi, a third year Computer Engineering major. I see ULA as an opportunity to help those around me and learn more in the process. In my free time, I love watching sports (KTBFFH !), comic books, and video games.",
   },
@@ -146,7 +144,13 @@ const ULAs: ULA[] = [
     name: "Livayaa",
     classes: "CS10ABC",
     image: Livayaa,
-    desc: "Hey! My name is Livayaa, and I’m a second-year Computer Science major. I joined ULA because I remember how much my own ULAs helped me feel more confident in class, and now I’d love to pass that same support forward. In my free time, I love reading, curating overly specific playlists, and watching Modern Family!",
+    desc: "Hey! My name is Livayaa, and I'm a second-year Computer Science major. I joined ULA because I remember how much my own ULAs helped me feel more confident in class, and now I’d love to pass that same support forward. In my free time, I love reading, curating overly specific playlists, and watching Modern Family!",
+  },
+  {
+    name: "Luis",
+    classes: "CS10ABC",
+    image: Luis,
+    desc: "Hi everyone! My name is Luis Bojorquez I'm a third year CSBA major. As a ULA I strive to help others, the same way others have helped me. I am interested in running, going to the gym, and I love One Piece.",
   },
   {
     name: "Mailyn",
@@ -155,10 +159,28 @@ const ULAs: ULA[] = [
     desc: "Hi everyone! My name is Mailyn and I'm a fourth year computer science major. I joined ULA because I really enjoy the program, and it always been really friendly and helpful to me when I came. I want to hopefully be able to do the same for other students. In my free time, I like to go to the gym and play the guitar!",
   },
   {
+    name: "Marilyn",
+    classes: "CS100",
+    image: Marilyn,
+    desc: "Hello! My name is Marilyn and I am a second year Computer Science major. I wanted to become a ULA because I enjoy being able to help others and share what I have learned with them. I love baking and spending time with my family. Feel free to reach out if you need help with anything or if you have any questions!",
+  },
+  {
     name: "Natalie",
     classes: "CS9ABC",
     image: Natalie,
     desc: "Hi, my name is Natalie! I'm a 4th-year CS major. I have always enjoyed helping others in school whether I had taken the class or not and ULA is a great place for me to help more people. I like to listen to music, take naps, and hang out with friends whenever I have free time.",
+  },
+  {
+    name: "Nikhil",
+    classes: "CS9ABC",
+    image: Nikhil,
+    desc: "Hi, my name is Nikhil Rao, and I’m a fourth-year Computer Science major. I wanted to become a ULA because I feel it’s the right time to give back to the same community that’s supported me throughout my journey here. Outside of school, I enjoy playing video games, going to the gym, listening to music, and watching shows and movies.",
+  },
+  {
+    name: "Pratheek",
+    classes: "CS10ABC",
+    image: Pratheek,
+    desc: "Hi, my name is Pratheek and I am a second year computer science major. I became a ULA because I wanted to help students the same way I received help from other ULAs in my freshman year. In my free time, I like playing badminton and watching soccer!",
   },
   {
     name: "Ramya",
@@ -167,10 +189,28 @@ const ULAs: ULA[] = [
     desc: "Hi, my name is Ramya and I'm a fourth year CS major. I wanted to be a ULA because I enjoy tutoring and I want to do what ULA did for me. In my free time I love to spend time with my dog!",
   },
   {
+    name: "Riley",
+    classes: "CS9ABC",
+    image: Riley,
+    desc: "Hi everyone! My name is Riley, and I'm a second-year Data Science major at UCR. I'm working as a ULA tutor this year, and I'm really looking forward to helping students and meeting new people. Outside of school, I enjoy playing basketball, making art, and just exploring new interests.",
+  },
+  {
     name: "Sahana",
     classes: "CS61, 100",
     image: Sahana,
     desc: "Hi! I'm Sahana, a third year computer science with business applications major! I joined ULA to be able to help a wider range of students in multiple classes. I love taking photos, crocheting, and reading :D Please feel free to reach out if you have any questions or you just want to talk!",
+  },
+  {
+    name: "Sarbesh",
+    classes: "CS9ABC, 10ABC",
+    image: Sarbesh,
+    desc: "Hi, my name is Sarbesh! I'm a third year computer science major. I joined the ULA program to help other students and share my knowledge. In my free time, I love to play basketball, go on runs, and watch YouTube! Please feel free to reach out about anything!",
+  },
+  {
+    name: "Simon",
+    classes: "CS61",
+    image: Simon,
+    desc: "Hi, my name is Simon! I'm a 2nd year Computer Science major. I like reading novels, watching shows, and sleeping. I also enjoy helping people, so feel free to ask me for help with your classes.",
   },
   {
     name: "Stanley",
